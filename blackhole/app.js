cd14f5bd1dd75f5e1a00866f0886f8df1d7afd8e
thestuckster <sstucky3@gmail.com>
2016-10-11 12:58:18 -0500

    health check function added (useful for debugging).

--- blackhole/app.js
+++ blackhole/app.js
@@ -1,12 +1,14 @@
-var express = require("express");
 const port = 8080;
-
-var app = express();
+var app = require("express")();
 var http = require("http").Server(app);
 
-
+//TODO: replace with real stuff.. 
 app.get("/", function(req, res) {
   res.send("Hello, World!");
+});
+
+app.get("/health", function(req, res) {
+  res.send("Sex, Drugs, Rock & Roll!");
 });
 
 app.listen(port, function() {
