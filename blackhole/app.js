df54d3dd0a3130f4da68a83f649b51df38d30e33
thestuckster <sstucky3@gmail.com>
2016-11-28 19:39:34 -0600

    fuck

--- blackhole/app.js
+++ blackhole/app.js
@@ -1,8 +1,20 @@
 const port = 8080;
+const SELECT_ALL = "SELECT * FROM cs420";
 var app = require("express")();
 var http = require("http").Server(app);
+var max_lat = 0.0;
+var max_long = 0.0;
+var min_lat = 0.0;
+var min_long = 0.0;
 
-//TODO: replace with real stuff..
+const pool = mysql.createPool({
+   connectionLimit: 25,
+   host: 'localhost',
+   user: 'node'
+   password: 'nodePassword'
+   database: 'cs420'
+ });
+
 app.get("/", function(req, res) {
   res.send("Hello, World!");
 });
@@ -12,7 +24,19 @@ app.get("/health", function(req, res) {
 });
 
 app.get("/getData", function(req, res) {
+  pool.getConnection((err, connection) => {
+    connection.query(SELECT_ALL, (err, rows, fields) =>{
 
+      if(err) {
+        console.log("fuck: " + err);
+      }
+
+
+
+      connection.release();
+
+    });
+  });
 });
 
 app.listen(port, function() {
