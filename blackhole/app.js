b5c023a1f44b545b54b7a4007f5db58f2393411f
thestuckster <sstucky3@gmail.com>
2016-11-28 19:10:19 -0600

    added mysql depen

--- blackhole/app.js
+++ blackhole/app.js
@@ -2,13 +2,17 @@ const port = 8080;
 var app = require("express")();
 var http = require("http").Server(app);
 
-//TODO: replace with real stuff.. 
+//TODO: replace with real stuff..
 app.get("/", function(req, res) {
   res.send("Hello, World!");
 });
 
 app.get("/health", function(req, res) {
   res.send("Sex, Drugs, Rock & Roll!");
+});
+
+app.get("/getData", function(req, res) {
+
 });
 
 app.listen(port, function() {
