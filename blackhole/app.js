d721d5121e40d0fe71d4b881d40ac84448ad9eb2
thestuckster <sstucky3@gmail.com>
2016-11-29 12:51:02 -0600

    slight clean up

--- blackhole/app.js
+++ blackhole/app.js
@@ -1,17 +1,16 @@
-const port = 8080;
-const SELECT_ALL = "SELECT * FROM cs420 WHERE LATNS BETWEEN ?AND ? LONGEW BETWEEN ? AND ?";
 var app = require("express")();
 var http = require("http").Server(app);
 var mysql = require("mysql");
 
-const max_lat = -200;
-const max_long = 1800;
+const port = 8080;
+const SELECT_BETWEEN = "SELECT * FROM cs420 WHERE LATNS BETWEEN ?AND ? LONGEW BETWEEN ? AND ?";
+
 const min_lat = 0.0;
+const max_lat = -200;
 const min_long = 1600;
+const max_long = 1800;
 
-var returnArray = [];
-
-const pool = mysql.createConnection({
+const connection = mysql.createConnection({
    host: 'localhost',
    user: 'node',
    password: 'nodePassword',
@@ -29,58 +28,32 @@ app.get("/health", function(req, res) {
 app.get("/getData", function(req, res) {
 
   console.log("REQUEST RECIEVED!!");
+  var returnArray = [];
 
-  pool.connect();
-
+  connection.connect();
     for(var lat = min_lat; lat >= max_lat; lat -= 0.5) {
-        //console.log("current loop @ : " + lat )
       for(var lon = min_long; lon <= max_long; lon += 0.5) {
+        //skip the first loop to prevent getting values between 0 and 0
           if(lat == min_lat && lon == min_long) {
             continue;
           }
 
-          pool.query(SELECT_ALL, [lat - 0.5, lat, lon - 0.5, lon], function(err, results) {
+          pool.query(SELECT_BETWEEN, [lat - 0.5, lat, lon - 0.5, lon], function(err, results) {
             if(err) {
               console.log("error: " + err);
             } else {
               var obj = {};
               obj["data"] = results.length;
 
+              delete results;
               returnArray.push(obj);
           }
         });
       }
     }
 
-    pool.end();
-
-
-
-  // pool.getConnection((err, connection) => {
-  //
-  //   for(var lat = min_lat; lat >= max_lat; lat -= 0.5) {
-  //       //console.log("current loop @ : " + lat )
-  //     for(var lon = min_long; lon <= max_long; lon += 0.5) {
-  //         if(lat == min_lat && lon == min_long) {
-  //           continue;
-  //         }
-  //
-  //         connection.query(SELECT_ALL, [lat - 0.5, lat, lon - 0.5, lon], function(err, results) {
-  //           if(err) {
-  //             console.log("error: " + err);
-  //           } else {
-  //             var obj = {};
-  //             obj["data"] = results.length;
-  //
-  //             returnArray.push(obj);
-  //             connection.release();
-  //         }
-  //       });
-  //     }
-  //   }
-  // });
-
-  console.log("DONE!");
+    connection.end();
+    res.send("done af"); //TODO: return array.
 });
 
 app.listen(port, function() {
