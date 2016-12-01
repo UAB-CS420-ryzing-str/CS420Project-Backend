ebbfbab422e9c3f05b6a802858ef5cacaac56367
thestuckster <sstucky3@gmail.com>
2016-11-30 18:50:58 -0600

    reworked /get/location

--- blackhole/app.js
+++ blackhole/app.js
@@ -8,7 +8,7 @@ const max_lat = -200;
 const min_long = 1600;
 const max_long = 1800;
 const pool = mysql.createPool({
-   connectionLimit: 25,
+   connectionLimit: 75,
    host: '45.55.77.74',
    user: 'node',
    password: 'nodePassword',
@@ -26,17 +26,22 @@ app.get("/health", function(req, res) {
   res.send("Sex, Drugs, Rock & Roll!");
 });
 
-/** Get ALL the data **/
-app.get("/get/data/count", function(req, res) {
+/** Only get the data between these coords **/
+app.get("/get/location/minLat/:minLat/maxLat/:maxLat/minLong/:minLong/maxLong/:maxLong", function(req, res) {
 
-  console.log("/get/data/count request recieved...");
+  var minLat = req.params.minLat * 10;
+  var maxLat = req.params.maxLat * 10;
+  var minLong = req.params.minLong * 10;
+  var maxLong = req.params.maxLong * 10;
 
-  for(var current_lat = min_lat; current_lat >= max_lat; current_lat -= 5) {
-    for(var current_long = min_long; current_long <= max_long; current_long += 5) {
+  console.log("/get/location request recieved minLat:" + minLat + " maxLat:" + maxLat + " minLong:" + minLong + " maxLong:" + maxLong);
+
+  for(var current_lat = maxLat; current_lat >= minLat; current_lat -= 5) {
+    for(var current_long = minLong; current_long <= maxLong; current_long += 5) {
       var lat_back_step = current_lat + 5;
       var long_back_step = current_long - 5;
 
-      var query = "SELECT COUNT(*) as rowCount FROM hurricane_data WHERE (LatNS BETWEEN " + current_lat + " AND " + lat_back_step + ") AND (LonEW BETWEEN " + long_back_step + " AND " + current_long + ");";
+      var query = "SELECT LatNS, LonEW, YYYYMMDDHH FROM hurricane_data WHERE (LatNS BETWEEN " + current_lat + " AND " + lat_back_step + ") AND (LonEW BETWEEN " + long_back_step + " AND " + current_long + ");";
       queryDbForAllData(query, res);
     }
   }
@@ -48,11 +53,12 @@ function queryDbForAllData(query, res) {
       if(err) {
         console.log("error: " + err);
       } else {
-
         var obj = {};
-        obj["data"] = results[0].rowCount;
+        obj["count"] = results.length;
+        obj["data"] = results;
         returnArray.push(obj);
         delete results;
+
         if(returnArray.length == 1600) {
           sendReturnArray(res);
         }
@@ -65,41 +71,8 @@ function queryDbForAllData(query, res) {
 function sendReturnArray(res) {
   console.log("Sending response!");
   res.send(returnArray);
-
   //reset return array
   returnArray = [];
-}
-
-/** Only get the data between these coords **/
-app.get("/get/location/minLat/:minLat/maxLat/:maxLat/minLong/:minLong/maxLong/:maxLong", function(req, res) {
-
-  var minLat = req.params.minLat;
-  var maxLat = req.params.maxLat;
-  var minLong = req.params.minLong;
-  var maxLong = req.params.maxLong;
-
-  console.log("/get/location request recieved minLat:" + minLat + " maxLat:" + maxLat + " minLong:" + minLong + " maxLong:" + maxLong);
-
-  var query = "SELECT LatNS, LonEW, YYYYMMDDHH FROM hurricane_data WHERE (LatNS BETWEEN " + minLat + " AND " + maxLat + ") AND (LonEW BETWEEN " + minLong + " AND " + maxLong + " ) ORDER BY LatNS, LonEW, YYYYMMDDHH;";
-  queryDBForLocationData(query, res);
-});
-
-function queryDBForLocationData(query, res) {
-  pool.getConnection((err, connection) => {
-    connection.query(query, function(err, results) {
-      if(err) {
-        console.log("error: " + err);
-      } else {
-          if(results.length === 0) {
-            console.log("No results. returning empty array");
-            res.send([]);
-          } else {
-            res.send(results);
-          }
-      }
-    connection.release();
-    });
-  });
 }
 
 app.listen(port, function() {
